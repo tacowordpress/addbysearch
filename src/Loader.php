9525c50ec080da6a34a546cea39a80c8597e85fd
Jasand <jasand@vermilion.com>
2015-02-14 01:54:20 -0700

    file serve - loading frontend file fix
    This workaround allows files to loaded from a path that is not
    accessible through the url. It’s essentially a proxy for files.

--- src/Loader.php
+++ src/Loader.php
@@ -6,7 +6,7 @@ class Loader
     public static function init()
     {
         add_action('admin_footer', 'AddBySearch\AddBySearch::init');
-        add_action('shutdown', 'AddBySearch\Frontend\Loader::addToHTML');
+        add_filter('parse_query', 'AddBySearch\Frontend\Loader::fileServe');
         return true;
     }
 }
