3347ca74b785a270c08dda6dedd5e14f1e9f6dfa
Jasand <jasand@vermilion.com>
2015-02-21 16:53:01 -0700

    trying to make fronted loader a seperate class

--- src/Loader.php
+++ src/Loader.php
@@ -1,12 +1,20 @@
 <?php
 namespace AddBySearch;
-
+use \FrontendLoader\FrontendLoader;
 class Loader
 {
     public static function init()
     {
         add_action('admin_footer', 'AddBySearch\AddBySearch::init');
-        add_filter('parse_query', 'AddBySearch\Frontend\Loader::fileServe');
+        add_filter('parse_query', function($query) {
+          $files = array('addbysearch.js', 'addbysearch.css', 'icon-move.png');
+          $front_end_loader = new FrontendLoader(
+            'addons/addbysearch',
+            dirname(__FILE__),
+            array('addbysearch.js', 'addbysearch.css', 'icon-move.png'));
+          $front_end_loader->fileServe($query);
+          return $query;
+        });
         return true;
     }
 }
