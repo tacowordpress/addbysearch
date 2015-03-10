891a02173fd139e2fbd2a0b4ca6d20f7b714c2bc
Jasand <jasand@vermilion.com>
2015-03-10 13:51:38 -0600

    namespacing

--- src/Loader.php
+++ src/Loader.php
@@ -5,7 +5,7 @@ class Loader
 {
     public static function init()
     {
-        add_action('admin_footer', 'AddBySearch\AddBySearch::init');
+        add_action('admin_footer', '\AddBySearch\AddBySearch::init');
         add_filter('parse_query', function($query) {
           
           $front_end_loader = new FrontendLoader(
